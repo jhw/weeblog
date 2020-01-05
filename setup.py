8a39b33c33d5f014ee6b176d1d8f1f3261e32a84
Justin <justin.worrall@gmail.com>
2020-01-05 20:25:52 +0000

    added readme, setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description=fh.read()
+    
+setuptools.setup(
+    name="weeblog",
+    version="0.0.1",
+    author="jhw",
+    author_email="justin.worrall@gmail.com",
+    description="Weeblog blogging framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jhw/weeblog",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
